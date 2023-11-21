32545a3f082f81565f067feb85809dc4cab1607d
Nicole Ngoc Gia Lam <nicolelam8891@gmail.com>
2023-11-20 18:41:29 -0700

    Add a successful API GET network request, able to see all data

---
+++ newsreader/src/apiCalls.js
@@ -0,0 +1,32 @@
+  // const getAllArticles = () => {
+  //   return fetch("https://newsapi.org/v2/everything?q=apple&from=2023-11-19&to=2023-11-19&sortBy=popularity&apiKey=0f379f20ac8f44c8b3d787d28a09635a")
+  //   .then((response) => {
+  //     if(!response.ok) {
+  //       throw new Error(`${response.status} ${response.statusText}`);
+  //     }
+  //     return response.json();
+  //   })
+  //   .then((data) => {
+  //     console.log("data:=====", data);
+  //     setArticles(data.articles);
+  //     console.log("data.articles:=====", data.articles);
+  //   })
+  //   .catch((event) => {
+  //     console.error(event.message);
+  //     setError(event.message);
+  //   })
+  // }
+  // useEffect(() => {
+  //   getAllArticles();
+  // }, []);
+
+
+// export default getAllArticles
+
+
+
+
+
+
+
+
--- newsreader/src/components/App/App.js
+++ newsreader/src/components/App/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import mockData from "../../MockData";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  const [articles, setArticles] = useState(mockData);
+  console.log("articles:=====", articles);
+  console.log("mockData:=====", mockData);
+  const [error, setError] = useState("");
+
+
+  return <main className='App'>
+    <Routes>
+      {/* <Route path="/" element={} /> */}
+    </Routes>
+  </main>;
+};
 
 export default App;
